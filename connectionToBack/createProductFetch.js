2a356b13d954d2f414d320117220ab22fe59b89e
Unknown <zerpa2007@hotmail.com>
2021-07-26 19:13:21 -0400

    Added createProductFecth

---
+++ connectionToBack/createProductFetch.js
@@ -0,0 +1,40 @@
+import { setRes } from './setGetRes';
+
+const createProductFetch = async ( name, dsc, price, item) => {
+  try {
+    const body = {
+      name: name,
+      dsc: dsc,
+      price: price,
+    };
+
+    const data = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
+
+    return  await fetch(`https://uber-eats-moviles.herokuapp.com/createproduct/${item}`, data)
+    .then((cat) => {
+      if (cat.status == 200) {
+        setRes('Success');
+      } else {
+        setRes('Fail');
+      }
+    })
+      .catch((error) => {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+        throw error;
+      })
+      .then((response) => {
+        console.log('Success: ', response);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default createProductFetch;
